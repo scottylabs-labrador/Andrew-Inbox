017e7f696a44426c047092aa1174585aa81a572e
brady-q <braydenquo@gmail.com>
2026-02-21 17:46:41 -0500

    canvas login with dummy info

--- selenium.js
+++ selenium.js
@@ -8,8 +8,27 @@ const {By, Builder, Browser} = require('selenium-webdriver');
     await driver.get('https://canvas.cmu.edu');
     let title = await driver.getTitle();
     console.log(title);
+    username = "Test";
+    password = "Test";
+
     await driver.manage().setTimeouts({implicit: 500});
-    let html = await driver.getPageSource();
+
+
+    let usernameInput = await driver.wait(
+      until.elementLocated(By.id(username)),
+      10000
+    ); 
+    await usernameInput.sendKeys(usernameInput);
+    
+    let passwordInput = await driver.wait(
+      until.elementLocated(By.id(password)),
+      10000
+    ); 
+    await passwordInput.sendKeys(passwordInput);
+    await passwordInput.sendKeys("\n");
+
+
+    // let html = await driver.getPageSource();
     // let textBox = await driver.findElement(By.tagName('h3'));
     // let text = await textBox.getAttribute("");
     // console.log(html);
