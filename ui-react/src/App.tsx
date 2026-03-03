5c33ba695035c5d9c7260d6a56e3cde479cf20e9
Ankita Varigonda <avarigonda18@gmail.com>
2026-03-02 19:55:26 -0500

    prevent resize

--- ui-react/src/App.tsx
+++ ui-react/src/App.tsx
@@ -1,12 +1,21 @@
 import "./App.css";
-import { Box } from "@chakra-ui/react";
-
+import { Box, VStack } from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <Box width="350px" height="500px" p={4}></Box>
+      <Box
+        width="350px"
+        height="500px"
+        overflow="hidden"
+        display="flex"
+        flexDirection="column"
+        bg="bg.panel"
+        position="relative"
+        p={4}
+      >
+        <VStack flex="1" overflowY="auto" p="4" align="stretch"></VStack>
+      </Box>
     </>
   );
 }
-
 export default App;
