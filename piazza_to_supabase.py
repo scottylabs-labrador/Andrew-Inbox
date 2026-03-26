fd0f7bb45c77502ab0e97640208b81bd44caecba
brady-q <braydenquo@gmail.com>
2026-03-25 20:35:34 -0400

    piazza and supabase stuff

---
+++ piazza_to_supabase.py
@@ -0,0 +1,45 @@
+from piazza import get_all_posts
+
+# IMPORTS
+# ===============================
+import requests
+from datetime import datetime, timezone
+from supabase import create_client, Client
+import json
+import os
+from dotenv import load_dotenv
+
+# ===============================
+# SUPABASE CONFIG
+# ===============================
+
+SUPABASE_URL = ""
+SUPABASE_KEY = ""
+
+# supabase_url = os.getenv(SUPABASE_URL)
+# supabase_key = os.getenv(SUPABASE_KEY)
+
+supabase_url = SUPABASE_URL
+supabase_key = SUPABASE_KEY
+
+supabase: Client = create_client(supabase_url, supabase_key)
+
+piazza_posts = get_all_posts()
+
+for post in piazza_posts:
+    announcement = {
+        "user_id": "pnimmaga",
+        "course": post["course"],
+        "title": post["title"],
+        "description": post["text"],
+        "link": post["url"],
+        "date": datetime.fromisoformat(post["date"]).astimezone(timezone.utc).isoformat(),
+        "platform": "piazza",
+        "is_read": False        
+    }
+    try:
+        response = supabase.table("annoucements").insert(announcement).execute()
+        # if response.get("status_code") not in [200, 201, 204]:
+        #     print("Supabase insert failed:", response)
+    except Exception as e:
+        print("Supabase insert exception:", e)
